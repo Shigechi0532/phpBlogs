64ff69f4650fff603d4aa5150adc03dc67a77011
Shigechi0532 <130873801+Shigechi0532@users.noreply.github.com>
2024-03-28 22:38:19 +0900

    connect.phpのDB_NAME訂正

--- lib/connect.php
+++ lib/connect.php
@@ -1,7 +1,7 @@
 <?php
 class connect{
-    const DB_NAME = "php";
-    const HOST = "220c65c035c68ab480c9ac44ac06a000e070f24e15fa56d19a3ef04697b03a97";
+    const DB_NAME = "phpBlogs";
+    const HOST = "db";
     const USER = "root";
     const PASS = "password";
 
@@ -12,6 +12,7 @@ class connect{
         try{
             // PDOのインスタンスをクラス変数に格納する
             $this->dbh = new PDO($dsn, self::USER, self::PASS);
+
         }catch(PDOException $e){
             // Exceptionが発生したら表示して終了
             exit($e->getMessage());
@@ -21,11 +22,11 @@ class connect{
         $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
-    public function query($sql,$parm=null){
+    public function query($sql,$param=null){
         // プリペア度ステートメントを作成し、SQL文を実行する準備をする
         $stmt = $this->dbh->prepare($sql);
         // パラメータを割り当てて実行し、結果セットを返す
-        $stmt->execute($parm);
+        $stmt->execute($param);
         return $stmt;
     }
 }
