57656237200fd22885cb3d47f8aae06c8171167d
Shigechi0532 <130873801+Shigechi0532@users.noreply.github.com>
2024-03-30 17:10:23 +0900

    投稿画面の作成

--- post.php
+++ post.php
@@ -55,7 +55,21 @@
         <div class="row">
             <div class="col-md-12">
 
-                <p>本文を入れる。</p>
+                <h1>記事の投稿</h1>
+
+                <form action="post.php" method="post">
+                    <div class="mb-3">
+                        <label class="form-label">タイトル</label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">本文</label>
+                        <textarea name="body" class="form-control" rows="10"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">投稿する</button>
+                    </div>
+                </form>
 
             </div>
         </div>
