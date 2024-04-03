4be0e639e4fbc03285a31d5d0316508f1d40caae
Shigechi0532 <130873801+Shigechi0532@users.noreply.github.com>
2024-04-03 22:07:44 +0900

    カテゴリー実装

--- lib/nav.php
+++ lib/nav.php
@@ -3,6 +3,7 @@
         <a class="navbar-brand" href="backend.php">My Blog Backed</a>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item"><a class="nav-link" href="category.php">カテゴリー</a></li>
                 <li class="nav-item"><a class="nav-link" href="post.php">記事を書く</a></li>
                 <li class="nav-item"><a href="logout.php">ログアウト</a></li>
             </ul>
